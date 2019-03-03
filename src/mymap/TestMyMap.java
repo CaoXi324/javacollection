313a0ee64d60e6bef9e3bc713d00a564c20133dc
Harper324 <1060010408@qq.com>
2019-03-03 21:58:26 +0800

    Modify MyMap with generics.

--- src/mymap/TestMyMap.java
+++ src/mymap/TestMyMap.java
@@ -1,17 +1,22 @@
 package mymap;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class TestMyMap {
 
     public static void main(String[] args) {
         MyMap myMap = new MyMap();
         myMap.put(1, "apple");
-        myMap.put(2, "banana");
+        myMap.put("3", 3);
         myMap.put(3, "cat");
         System.out.println(myMap.size);
         myMap.replace(2, "rbanana");
         System.out.println(myMap.get(2));
         System.out.println(myMap.get(1));
+        System.out.println(myMap.get("3"));
         System.out.println(myMap.containsValue("apple"));
         System.out.println(myMap.containsKey(2));
+
     }
 }
